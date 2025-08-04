e28f965478eec8c96f125e08e754c70ba41ede0a
grit11 <105155813+grit11@users.noreply.github.com>
2025-08-04 22:17:03 +0800

    Delete 大模型部署/代码/download.py

--- 大模型部署/代码/download.py
+++
@@ -1,8 +0,0 @@
-import os
-from modelscope import snapshot_download
-
-# 设置国内镜像源
-os.environ["MODEL_SCOPE_REPO_MIRROR"] = "https://mirror.modelscope.cn"
-
-# 下载模型（自动从国内镜像加速）
-model_dir = snapshot_download('Qwen/Qwen2.5-VL-72B-Instruct', cache_dir='./')
